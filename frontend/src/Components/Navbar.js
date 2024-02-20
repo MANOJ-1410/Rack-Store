f23337c3d44764004b7a5d35715447368ae1dc85
MANOJ-1410 <98142288+MANOJ-1410@users.noreply.github.com>
2024-02-20 14:22:50 +0530

    push

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -19,7 +19,7 @@
 //     setName(null);
 //     navigate('/login');
 //   }, [navigate]);
-  
+
 //   useEffect(() => {
 //     const token = localStorage.getItem('token');
 //     if(token){
@@ -30,15 +30,15 @@
 //       setName(null);
 //     }
 
- 
+
 //   }, [navigate,handleLogout])
-  
+
 
 //   return (
 //     <div className= { styles.navbar_bg }>
 //       <div className= { styles.container} >
 
-     
+
 //       <div className={ styles.content}>
 //         <div className={ styles.logo_img}>
 //           <img src="./D2R logo.jpeg" alt="Img" className={ styles.logo_img_src} />
@@ -68,7 +68,7 @@
 //           <div className={ styles.logout} onClick={handleLogout}>Logout</div>
 //         </div>
 
-        
+
 //         </>
 //       ):
 //       (name) ? (
@@ -80,10 +80,10 @@
 //         <Link to="/cart" className={ styles.cartNav_container}><BiCartAlt size={30}/>{cartCount}</Link>
 //         <span className={ styles.right_line}></span>
 //         <Link to={'/history'} className={ styles.admin_view}>History</Link>
-        
+
 //       </div>
 
-  
+
 //         <div className={styles.flexCenter}>
 //           <div><CgProfile size={20}/></div>
 //           <div>{name}</div>
@@ -210,7 +210,7 @@
 //     setName(null);
 //     navigate('/login');
 //   }, [navigate]);
-  
+
 //   useEffect(() => {
 //     const token = localStorage.getItem('token');
 //     if(token){
@@ -221,18 +221,18 @@
 //       setName(null);
 //     }
 
- 
+
 //   }, [navigate,handleLogout])
 
 
-  
+
 // return (
 //   <nav className={styles.navbar_container}>
 //     <div className={styles.name}>
 //       <div className="line"></div>
 //       <div className="line"></div>
 //       <div className="line"></div>
-   
+
 //     <button
 // 					className="nav-btn nav-close-btn"
 // 					onClick={showNavbar}>
@@ -249,7 +249,7 @@
 //       <div className={styles.navbar_center}>
 
 //         <Link to={'/home'} className={styles.admin_view}>
-       
+
 //           {/* {<img src='./bx-home-alt-2.svg' alt=''></img> } */}
 //           <box-icon name='home-alt-2' animation='tada' ></box-icon>
 //         </Link>
@@ -296,129 +296,144 @@
 //   </nav>
 // );
 // }
-
-
-
 // export default Navbar;
 
 
+
+
+
+
+// new navbar designs
+
 import React, { useCallback, useEffect, useState } from 'react';
 import styles from './Navbar.module.css';
+import { GiHamburgerMenu } from "react-icons/gi";
 import { Link, useNavigate } from 'react-router-dom';
 import { useCart } from '../Context/CartContext';
 import { BiCartAlt } from 'react-icons/bi';
 import { jwtDecode } from 'jwt-decode';
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from "react";
-
+// import 'boxicons'; // Import BoxIcons library
+// import 'boxicons/css/boxicons.min.css'; // Import BoxIcons CSS
 
 const Navbar = () => {
   const { cartCount } = useCart();
   const [name, setName] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
   const navigate = useNavigate();
-  const navRef = useRef();
 
-  const [showNavbarCenter, setShowNavbarCenter] = useState(false);
-
-  const showNavbar = () => {
-      setShowNavbarCenter(!showNavbarCenter);
-  };
-  
-  const toggleNavbarCenter = () => {
-    setShowNavbarCenter(!showNavbarCenter);
-};
-
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
 
   const handleLogout = useCallback(() => {
     localStorage.removeItem('token');
     setName(null);
     navigate('/login');
   }, [navigate]);
-  
+
   useEffect(() => {
     const token = localStorage.getItem('token');
-    if(token){
+    if (token) {
       const decodedToken = jwtDecode(token);
-      setName(decodedToken.name);
-      setIsAdmin(decodedToken.isAdmin);
+      setName(decodedToken.name)
+      setIsAdmin(decodedToken.isAdmin)
     } else {
       setName(null);
     }
-  }, [navigate, handleLogout]);
+
+
+  }, [navigate, handleLogout])
+
+
 
   return (
-    <nav className={styles.navbar_container} ref={navRef}>
-      <div className={styles.name} onClick={showNavbar}>
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-        <button className="nav-btn nav-close-btn">
-          <FaTimes />
-        </button>
-      </div>
 
-      <div className={styles.logo_img}>
+    <nav className={styles.main_nav}>
+      {/* 1st logo part  */}
+      <div className={styles.logo}>
         <img src="./D2R logo.jpeg" alt="Img" className={styles.logo_img_src} />
         <h2>D2R</h2>
       </div>
 
-      {showNavbarCenter && (
-    <div className={styles.navbar_center_below}>
-        <Link to={'/home'} className={styles.admin_view}>
-            <box-icon name='home-alt-2' animation='tada' ></box-icon>
-        </Link>
-        <Link to={'/admin-orders'} className={styles.admin_view}>View Orders</Link>
-        <Link to={'/admin-create'} className={styles.admin_view}>Create Rack</Link>
-        <Link to={'/admin-manage'} className={styles.admin_view}>Manage Racks</Link>
-        <Link to={''} className={styles.admin_view}>Random 1</Link>
-        <Link to={''} className={styles.admin_view}>Random 2</Link>
-    </div>
-)}
+      {/* 2nd menu part  */}
+      <div className={showMediaIcons ? `${styles.menu_link} mobile_menu_link` : 'menu_link'}>
 
+        {(isAdmin && name) ? (
+          <>
+            <div className={styles.menu_link}>
+              <ul>
+                <li>
+                  <Link to={'/home'} className={styles.admin_view}>
+                    {/* <box-icon name='home-alt-2' animation='tada'></box-icon> */}
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                      <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                      <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                    </svg>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={'/admin-orders'} className={styles.admin_view}>View Orders</Link>
+                </li>
+                <li>
+                  <Link to={'/admin-create'} className={styles.admin_view}>Create Rack</Link>
+                </li>
+                <li>
+                  <Link to={'/admin-manage'} className={styles.admin_view}>Manage Racks</Link>
+                </li>
+                <li>
+                  <Link to={''} className={styles.admin_view}>Random 1</Link>
+                </li>
+                <li>
+                  <Link to={''} className={styles.admin_view}>Random 2</Link>
+                </li>
+              </ul>
+            </div>
 
-      {isAdmin && name ? (
-        <div className={styles.navbar_center}>
-          <Link to={'/home'} className={styles.admin_view}>
-            <box-icon name='home-alt-2' animation='tada' ></box-icon>
-          </Link>
-          <Link to={'/admin-orders'} className={styles.admin_view}>View Orders</Link>
-          <Link to={'/admin-create'} className={styles.admin_view}>Create Rack</Link>
-          <Link to={'/admin-manage'} className={styles.admin_view}>Manage Racks</Link>
-          <Link to={''} className={styles.admin_view}>Random 1</Link>
-          <Link to={''} className={styles.admin_view}>Random 2</Link>
-        </div>
-        
-      ) : name ? (
-        <div className={styles.navigators}>
-          <Link to="/">Home</Link> 
-          <Link to="/cart" className={styles.cartNav_container}><BiCartAlt size={30} />{cartCount}</Link>
-          <Link to={'/history'} className={styles.admin_view}>History</Link>
-        </div>
-      ) : (
-        <div className={styles.flexCenter}>
-          <Link to="/login" className={styles.login}>Login</Link>
-          <Link to="/signup" className={styles.signup}>Signup</Link>
-        </div>
-      )}
+            {/* 3rd social media links */}
 
-      {name && (
-        <div className={styles.flexCenter}>
-          <img src="./images.png" alt="User-profile"/>
-          <span>{name}</span>
-          <i class='bx bx-caret-down'></i>
-          <div className={styles.logout} onClick={handleLogout}>Logout</div>
-          <button className="nav-btn" onClick={showNavbar}>
-            <FaBars />
-          </button>
-          <button className="nav-btn nav-close-btn" onClick={toggleNavbarCenter}>
-    <FaTimes />
-</button>
+            <div className={styles.social_media}>
+              <img src="./images.png" alt="User-profile" />
+              <span>{name}</span>
+              <div className={styles.logout} onClick={handleLogout}>Logout</div>
 
-        </div>
-      )}
+              {/* hamburget menu start  */}
+              <div className={styles.hamburger_menu}>
+                <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                  <GiHamburgerMenu />
+                </a>
+              </div>
+            </div>
+          </>
+        ) :
+          (name) ? (
+            <>
+              <div className={styles.navigators}>
+                <Link to="/">Home</Link>
+                <Link to="/cart" className={styles.cartNav_container}><BiCartAlt size={30} />{cartCount}</Link>
+                <Link to={'/history'} className={styles.admin_view}>History</Link>
+              </div>
+
+            </>
+          ) :
+            (
+              <>
+
+                <div className={styles.flexCenter}>
+                  <img src="./images.png" alt="User-profile" />
+                  <span>{name}</span>
+                  <Link className='logout' onClick={handleLogout}>Logout</Link>
+                </div>
+
+                <div className={styles.flexCenter}>
+                  <Link to="/login" className={styles.login}>Login</Link>
+                  <Link to="/signup" className={styles.signup}>Signup</Link>
+                </div>
+              </>
+            )}
+      </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
+
